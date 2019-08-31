71503caaf74781f7c48a0b907f01f5d92aed8d29
梦魇劫烬 <2621290200@qq.com>
2019-08-31 19:46:17 +0800

    初始化完成

--- src/main.js
+++ src/main.js
@@ -2,6 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import ElementUI from 'element-ui'
+import '../node_modules/element-ui/lib/theme-chalk/index.css'
+
+import axios from 'axios'
+Vue.prototype.$axios = axios
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0/'
+
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({

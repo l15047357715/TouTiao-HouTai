0fdc93f1f9082b2708a66e48ebd8419c36895d2a
梦魇劫烬 <2621290200@qq.com>
2019-09-03 18:48:14 +0800

    0.61

--- src/router.js
+++ src/router.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import Home from './views/home'
 import Login from './views/login'
-import Main from './views/home/main.vue'
+import Home from './views/home'
+
+import Main from './views/home/main'
 
 Vue.use(Router)
 
